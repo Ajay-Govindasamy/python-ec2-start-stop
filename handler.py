d3b225f7abf6185d785805fd08a2609fd86617d9
Ajay-Govindasamy <63512607+Ajay-Govindasamy@users.noreply.github.com>
2021-03-31 10:28:38 +0100

    added comments

--- handler.py
+++ handler.py
@@ -1,8 +1,9 @@
 import boto3
 
+# boto3, python SDK for accessing AWS services
 ec2 = boto3.client('ec2')
 
-
+# starting an ec2 instance
 def start_ec2(event, context):
     ec2_instances = get_all_ec2_ids()
     response = ec2.start_instances(
@@ -11,7 +12,7 @@ def start_ec2(event, context):
     )
     return response
 
-
+# stopping an instance
 def stop_ec2(event, context):
     ec2_instances = get_all_ec2_ids()
     response = ec2.stop_instances(
@@ -21,13 +22,13 @@ def stop_ec2(event, context):
     return response
 
 
-# get the list of all the ec2 instances
+# get the list of all the ec2 instances currently available
 def get_all_ec2_ids():
     response = ec2.describe_instances(DryRun=False)
     instances = []
     for reservation in response["Reservations"]:
         for instance in reservation["Instances"]:
-            # This sample print will output entire Dictionary object
+            # prints the entire Dictionary object
             # This will print will output the value of the Dictionary key 'InstanceId'
             instances.append(instance["InstanceId"])
     return instances
